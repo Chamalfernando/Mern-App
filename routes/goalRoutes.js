85ac41ac4749d45508b4acc49a328048e54abdba
Chamalfernando <chamaldeshitha2001@gmail.com>
2024-07-21 02:29:49 +0530

    commit message for partially done jwt auth

--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -4,13 +4,15 @@ const {getGoals,setGoal,updateGoal,deleteGoal} = require('../controllers/goalCon
 // const { check, validationResult } = require('express-validator');
 // const auth = require('../middleware/auth');
 
-router.route('/').get(getGoals).post(setGoal); // the below commented lines will be replaced by this.
+const {protect} = require("../middleware/authMiddleware")
+
+router.route('/').get(protect,getGoals).post(protect,setGoal); // the below commented lines will be replaced by this.
 
 // router.get("/", getGoals); 
 
 // router.post("/",setGoal); 
 
-router.route("/:id").put(updateGoal).delete(deleteGoal); // the below commented lines will be replaced by this.
+router.route("/:id").put(protect,updateGoal).delete(protect,deleteGoal); // the below commented lines will be replaced by this.
 
 // router.put("/:id",updateGoal); 
 
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
 const goalRoutes = require("./routes/goalRoutes");
+const userRoutes = require("./routes/userRoutes")
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals",goalRoutes)
+app.use("/api/users",userRoutes)
 
 app.use(errorHandler); // will override the default express error handler.
 
